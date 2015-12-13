76bfb4660523127ad2108e957ea9282cc4606153
Adrian Ruiz <aerf2007@gmail.com>
2015-12-13 02:06:19 -0500

    On Click Welcome

--- app/src/main/java/ysiparticipo/itnovate/com/ysiparticipo/WelcomeActivity.java
+++ app/src/main/java/ysiparticipo/itnovate/com/ysiparticipo/WelcomeActivity.java
@@ -1,13 +1,26 @@
 package ysiparticipo.itnovate.com.ysiparticipo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageView educacion_cultura =(ImageView)findViewById(R.id.educacion_cultura);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_welcome);
     }
+
+    public void abrirActivity(View view){
+        startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+    }
+
 }
