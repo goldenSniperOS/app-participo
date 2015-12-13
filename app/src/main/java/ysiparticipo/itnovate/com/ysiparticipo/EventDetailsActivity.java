432327ba52f8da0b6f65d4e46bff027f317376fb
Angel Sirlopu C <design.anguel@gmail.com>
2015-12-13 09:08:52 -0500

    Send Participacion

--- app/src/main/java/ysiparticipo/itnovate/com/ysiparticipo/EventDetailsActivity.java
+++ app/src/main/java/ysiparticipo/itnovate/com/ysiparticipo/EventDetailsActivity.java
@@ -1,26 +1,52 @@
 package ysiparticipo.itnovate.com.ysiparticipo;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.backendless.files.BackendlessFile;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import ysiparticipo.itnovate.com.ysiparticipo.Adapters.EventAdapter;
 
@@ -28,6 +54,11 @@ public class EventDetailsActivity extends AppCompatActivity {
     private VolleyS volley;
     protected RequestQueue fRequestQueue;
     Toolbar toolbar;
+    private static final int TAKE_PICTURE = 1;
+    private Uri imageUri;
+    BackendlessUser current;
+    private Integer item;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +66,7 @@ public class EventDetailsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         volley = VolleyS.getInstance(this);
         fRequestQueue = volley.getRequestQueue();
-        int item = intent.getIntExtra("item",1);
+        item = intent.getIntExtra("item",1);
         setContentView(R.layout.activity_event_details);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,8 +75,7 @@ public class EventDetailsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                takePhoto(view);
             }
         });
         String urlpost = "http://172.16.107.194/api-participo/home/evento/"+ item;
@@ -58,7 +88,7 @@ public class EventDetailsActivity extends AppCompatActivity {
                             TextView lugar = (TextView) findViewById(R.id.lugar);
                             lugar.setText(response.getString("LUGAR"));
                             TextView hora = (TextView) findViewById(R.id.hora);
-                            hora.setText(response.getString("FECHA")+ "-" + response.getString("HORA"));
+                            hora.setText(response.getString("FECHA") + "-" + response.getString("HORA"));
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -75,6 +105,105 @@ public class EventDetailsActivity extends AppCompatActivity {
         addToQueue(req);
     }
 
+
+    public void takePhoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    current = Backendless.UserService.CurrentUser();
+                    Toast.makeText(getApplicationContext(),current.getEmail()+"",Toast.LENGTH_LONG).show();
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    MaterialDialog dialog = new MaterialDialog.Builder(EventDetailsActivity.this)
+                            .title("Yo SI participo, de este evento")
+                            .customView(R.layout.camera_make, true)
+                            .positiveText("YO SI PARTICIPO")
+                            .negativeText(android.R.string.cancel)
+                            .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                    DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+                                    Date date = new Date();
+                                    String nameFile = current.getEmail()+"_"+ dateFormat.format(date) +".png";
+                                    Backendless.Files.Android.upload( bitmap, Bitmap.CompressFormat.PNG, 100, nameFile , "media", new AsyncCallback<BackendlessFile>()
+                                    {
+                                        @Override
+                                        public void handleResponse( final BackendlessFile backendlessFile )
+                                        {
+
+                                            final String url = backendlessFile.getFileURL();
+                                            HashMap<String, String> mRequestParams = new HashMap<String, String>();
+                                            mRequestParams.put("id_usuario", current.getEmail());
+                                            mRequestParams.put("nombre",current.getProperty("name").toString());
+                                            mRequestParams.put("id_evento",item+"");
+                                            mRequestParams.put("categoria","CULTURA");
+                                            mRequestParams.put("foto", url);
+                                            JSONObject obj = new JSONObject();
+                                            try {
+                                                obj.put("id_usuario", current.getEmail());
+                                                obj.put("nombre",current.getProperty("name").toString());
+                                                obj.put("id_evento", item+"");
+                                                obj.put("categoria", "CULTURA");
+                                                obj.put("foto", url);
+                                            } catch (JSONException e) {
+                                                // TODO Auto-generated catch block
+                                                e.printStackTrace();
+                                            }
+                                            String urlpost = "http://172.16.107.194/api-participo/home/participacion";
+                                            JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, urlpost,
+                                                    obj,
+                                                    new Response.Listener<JSONObject>() {
+                                                        @Override
+                                                        public void onResponse(JSONObject response) {
+                                                            Toast.makeText(getApplication(),"url: "+response,Toast.LENGTH_LONG).show();
+                                                        }
+                                                    }, new Response.ErrorListener() {
+                                                @Override
+                                                public void onErrorResponse(VolleyError error) {
+                                                    VolleyLog.e("Error: ", error.getMessage());
+                                                }
+                                            });
+                                            setSupportActionBar(toolbar);
+                                            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                                            imageView.setImageResource(R.drawable.eventos);
+                                            addToQueue(req);
+                                        }
+
+                                        @Override
+                                        public void handleFault( BackendlessFault backendlessFault )
+                                        {
+
+                                        }
+                                    });
+                                }
+                            }).build();
+                    ImageView imageView = (ImageView) dialog.getCustomView().findViewById(R.id.imageView10);
+                    ContentResolver cr = getContentResolver();
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+                        imageView.setImageBitmap(bitmap);
+                        dialog.show();
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load" + e.getMessage(), Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
+        }
+    }
+
     public void addToQueue(Request request) {
         if (request != null) {
             request.setTag(this);
@@ -88,6 +217,7 @@ public class EventDetailsActivity extends AppCompatActivity {
         }
     }
 
+
     public void onPreStartConnection() {
         this.setProgressBarIndeterminateVisibility(true);
     }
@@ -95,5 +225,7 @@ public class EventDetailsActivity extends AppCompatActivity {
     public void onConnectionFinished() {
         this.setProgressBarIndeterminateVisibility(false);
     }
+
+
 
 }
--- app/src/main/java/ysiparticipo/itnovate/com/ysiparticipo/WelcomeActivity.java
+++ app/src/main/java/ysiparticipo/itnovate/com/ysiparticipo/WelcomeActivity.java
@@ -5,15 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-
+    BackendlessUser current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ImageView educacion_cultura =(ImageView)findViewById(R.id.educacion_cultura);
-
+        current = Backendless.UserService.CurrentUser();
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_welcome);
